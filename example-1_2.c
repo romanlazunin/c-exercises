d3aa161897992b8a23ae947453b27bedc3fdffc9
Roman Lazunin <rlazunin@gmail.com>
2018-01-28 23:24:07 +0100

    completed exemple 1.2 - celcius to fahrenheit conversion with while loop

--- example-1_2.c
+++ example-1_2.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 
-main(){
-    printf("Example 1.2\n");
+/* print Fahrenheit-Celsius table
+    for fahr = 0, 20, ..., 300 */
+
+main()
+{
+    printf("Example 1.2\n\n");
+
+    int fahr, celcius;
+    int lower, upper, step;
+
+    lower = 0;      /* lower limit of temperature table */
+    upper = 300;    /* upper limit */
+    step = 20;      /* step size */
+
+    fahr = lower;
+    while (fahr <= upper) {
+        celcius = 5 * (fahr - 32) / 9;
+        printf("%d\t%d\n", fahr, celcius);
+        fahr = fahr + step;
+    }
 
     return 0;
 }
